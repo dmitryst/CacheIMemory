72bf33944dc8df407293fb72e48f64846f634790
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2018-09-02 11:21:53 +0300

    GetValueValue and Set

---
+++ CacheInMemoryAspNetCoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheInMemoryAspNetCoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMemoryCache _cache;
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        public IActionResult CacheTryGetValueSet()
+        {
+            DateTime cacheEntry;
+
+            // look for cache key
+            if (!_cache.TryGetValue(CacheKeys.Entry, out cacheEntry))
+            {
+                // key not in cache, so get data
+                cacheEntry = DateTime.Now;
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    // keep in cache for this time, reset time if accessed
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(5));
+
+                // save data in cache
+                _cache.Set(CacheKeys.Entry, cacheEntry, cacheEntryOptions);
+            }
+
+            return View("Cache", cacheEntry);
+        }
+    }
+}
--- CacheInMemoryAspNetCoreWeb/Startup.cs
+++ CacheInMemoryAspNetCoreWeb/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CacheInMemoryAspNetCoreWeb
@@ -15,6 +16,9 @@ namespace CacheInMemoryAspNetCoreWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +29,13 @@ namespace CacheInMemoryAspNetCoreWeb
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=CacheTryGetValueSet}/{id?}"
+                    );
             });
+
         }
     }
 }
